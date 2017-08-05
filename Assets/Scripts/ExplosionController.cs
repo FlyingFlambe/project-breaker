34a19c060910364b0736792066658712ccabfc11
Robert Desrosiers <robert.desrosiers@teamgiga.com>
2017-08-05 19:45:13 -0400

    initial setup

---
+++ Assets/Scripts/ExplosionController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionController : MonoBehaviour {
+
+    public PlayerController player;
+    public EnemyController enemy;
+
+    public Transform explosion;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<PlayerController>();
+        enemy = GetComponent<EnemyController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Animate()
+    {
+        Instantiate(explosion, transform.position, Quaternion.Euler(0, 0, Random.Range(0, 360)));
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] BorderParent;
+    public GameObject[] enemy;
+
+    //
+    void Awake()
+    {
+        foreach (GameObject border in BorderParent)
+            Instantiate(border);   
+    }
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Scene loadedLevel = SceneManager.GetActiveScene();
+
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(loadedLevel.buildIndex);
+    }
+}
